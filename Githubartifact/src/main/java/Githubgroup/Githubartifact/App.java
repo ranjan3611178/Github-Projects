ef34371bba79388a07ffc5331e074feacc91612e
Ranjan <Ranjan@Ranjan-PC>
2019-01-08 19:58:38 +0530

    Github test demo script

---
+++ Githubartifact/src/main/java/Githubgroup/Githubartifact/App.java
@@ -0,0 +1,13 @@
+package Githubgroup.Githubartifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Githubartifact/src/test/java/Githubgroup/Githubartifact/Git_demo1.java
@@ -0,0 +1,35 @@
+package Githubgroup.Githubartifact;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Git_demo1 {
+	 WebDriver driver;
+	
+	  @Test
+	  public void verifytitle() {
+		  driver.get("https://www.facebook.com/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Facebook – log in or sign up"));
+	  }
+	  @BeforeTest
+	  public void beforeTest() {
+		  System.out.println("--------*****open chrome browser******--------");
+		  String Path="F:\\SELENIUM\\Driver\\geckodriver-v0.23.0-win64\\geckodriver.exe";
+			System.setProperty("webdriver.gecko.driver", Path);
+			driver=new FirefoxDriver();  
+		  
+	  }
+
+	  @AfterTest
+	  public void afterTest() {
+		  driver.quit();
+	  }
+
+	
+
+}
